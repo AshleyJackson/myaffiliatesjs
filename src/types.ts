4aeb9b96f685e6f7c9124236b6d809f163962c6b
Ashley Jackson <Ashley@AshleyJackson.net>
2024-08-28 21:59:15 +1000

    types

---
+++ src/types.ts
@@ -0,0 +1,14 @@
+export interface Token {
+    prefix: string;
+    user_id: string;
+    setup_id: string;
+    plan_id: string;
+    media_id: string;
+    banner_id: string;
+    campaign_id: string;
+    authcode: string;
+    referring_url: string;
+    ip_address: string;
+    country: string;
+    time_stamp: Date;
+}
